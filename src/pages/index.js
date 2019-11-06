162c8a26132815a560a9e4d42a81594cc37ffb62
arpibb <bencze.arpad.21@gmail.com>
2019-11-06 14:13:18 +0100

    hotfix after fade-out done

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,9 @@ const IndexPage = () => (
     <div className="img-main-container">
         <img className="img-main" src={"https://timelinecovers.pro/facebook-cover/download/yoga-bohemian-colorful-mandala-facebook-cover.jpg"} alt={"There is nothing to see here."} />
     </div>
-<<<<<<< HEAD
-=======
     <div className="img-main-container">
         <img className="img-main" src={"https://timelinecovers.pro/facebook-cover/download/yoga-bohemian-colorful-mandala-facebook-cover.jpg"} alt={"There is nothing to see here."} />
     </div>
->>>>>>> 50561323f879e04d668f9d2e1dca5bc43e3ba14c
   </Layout>
 )
 
